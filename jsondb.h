7fbdb82cccc5fcf84ef45943ef8a6a0da66b90ea
Erdwig <rl.foerster.2002@gmail.com>
2022-03-11 10:24:31 +0100

    New API. Contains more set operations and cleaned some stuff up.

--- jsondb.h
+++ jsondb.h
@@ -38,17 +38,27 @@ struct jsondb_set {
     size_t size;
 };
 
+typedef int (*jsondb_cond_func)(jsondb_ref * ref, void * custom_env);
 
-void jsondb_init(void);
-void jsondb_deinit(void);
 
-void jsondb_add(char * json, char * json_end);
-struct jsondb_set jsondb_get(char * path);
-struct jsondb_set jsondb_select_eq(char *path, jsondb_ref *val);
 
-struct jsondb_set jsondb_set_get(struct jsondb_set * set, char *path);
-struct jsondb_set jsondb_set_select_eq(struct jsondb_set * set, char *path, jsondb_ref *val);
+void                jsondb_init             (void);
+void                jsondb_deinit           (void);
 
-void jsondb_set_free(struct jsondb_set * set);
+void                jsondb_add              (char * json);
+struct jsondb_set   jsondb_get              (char * path);
+struct jsondb_set   jsondb_select_eq        (char * path, struct jsondb_set * choices);
+struct jsondb_set   jsondb_select_cond      (jsondb_cond_func cond, void * custom_env);
+
+struct jsondb_set   jsondb_set_single       (char * json);
+void                jsondb_set_join         (struct jsondb_set * into, struct jsondb_set * from);
+struct jsondb_set   jsondb_set_get          (struct jsondb_set * set, char * path);
+struct jsondb_set   jsondb_set_select_eq    (struct jsondb_set * set, char * path, struct jsondb_set * choices);
+struct jsondb_set   jsondb_set_select_cond  (struct jsondb_set * set, jsondb_cond_func cond, void * custom_env);
+struct jsondb_set   jsondb_set_union        (struct jsondb_set * a, struct jsondb_set * b);
+struct jsondb_set   jsondb_set_inter        (struct jsondb_set * a, struct jsondb_set * b);
+struct jsondb_set   jsondb_set_diff         (struct jsondb_set * set, struct jsondb_set * sub);
+int                 jsondb_set_is_empty     (struct jsondb_set * set);
+void                jsondb_set_clear        (struct jsondb_set * set);
 
 #endif
