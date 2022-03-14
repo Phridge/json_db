8d758227bc00635ac0c38f84ad291768f010fccf
Richard Förster <richard.foerster@ivi.fraunhofer.de>
2022-03-14 19:43:47 +0100

    Server sources initial commit + thoughts

---
+++ server.h
@@ -0,0 +1,13 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+
+/* Server application for the DB.
+ * Uses/Supports Protocols:
+ * - idk
+ * Basically, there comes a json document in, and there goes one out.
+ * Where can you find something that simple? Make own Protocol on top of TCP?
+ */
+
+
+#endif
